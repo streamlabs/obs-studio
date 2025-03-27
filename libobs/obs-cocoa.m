8a02a1b943c2df6469490a737a59f15503754b8a
Richard Osborne <rosborne255@yahoo.com>
2025-03-27 10:09:48 -0500

    Fixed compile error where fail section never executed
    Looking at release/30 branch, the failure case is supposed to goto the "fail" area

--- libobs/obs-cocoa.m
+++ libobs/obs-cocoa.m
@@ -714,6 +714,7 @@ static bool init_hotkeys_platform(obs_hotkeys_platform_t **plat_)
 
     if (!plat->layout_data) {
         blog(LOG_ERROR, "hotkeys-cocoa: Failed getting LayoutData");
+        goto fail;
     } else {
         CFRetain(plat->layout_data);
         plat->layout = (UCKeyboardLayout *) CFDataGetBytePtr(plat->layout_data);
