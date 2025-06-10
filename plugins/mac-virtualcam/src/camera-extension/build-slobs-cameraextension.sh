1edc9fb5e7d4b9cbc87dc0202bc8a04517aa3361
Richard Osborne <rosborne255@yahoo.com>
2025-06-09 20:24:45 -0500

    Revert "reuse CmakePresets.json from the top level"
    This reverts commit f310a9fa1351893e2620e9be0b8a32f126ca99d4.

--- plugins/mac-virtualcam/src/camera-extension/build-slobs-cameraextension.sh
+++ plugins/mac-virtualcam/src/camera-extension/build-slobs-cameraextension.sh
@@ -1,7 +1,5 @@
 # Use custom SLOBS CmakeLists.txt file which directly builds the SystemExtension (instead of the default CMakeLists.txt file)
 
-cp -v ../../../../CMakePresets.json ./  # copy this file locally. We need to reuse the VIRTUALCAM_UUIDs
-
 # backup the default CMakeLists.txt file we do not wish to run.
 cp CMakeLists.txt backup-CMakeLists.txt
 
@@ -13,4 +11,3 @@ cmake --preset macos
 cmake --build build_macos --preset macos
 cp backup-CMakeLists.txt CMakeLists.txt
 rm backup-CMakeLists.txt
-rm CMakePresets.json
