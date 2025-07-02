3b8e513e00989d438a73c5802075716a7edbf126
mhoyer-streamlabs <mhoyer@logitech.com>
2025-07-02 07:47:12 -0500

    Update helpers.cmake

--- cmake/windows/helpers.cmake
+++ cmake/windows/helpers.cmake
@@ -30,7 +30,7 @@ function(set_target_properties_obs target)
     if(target STREQUAL obs-browser-helper)
       set(OBS_EXECUTABLE_DESTINATION "${OBS_PLUGIN_DESTINATION}")
     elseif(target STREQUAL win-streamlabs-vst)
-      set(OBS_EXECUTABLE_DESTINATION "${OBS_PLUGIN_DESTINATION}")    
+      set(OBS_EXECUTABLE_DESTINATION "${OBS_PLUGIN_DESTINATION}")
     elseif(target STREQUAL inject-helper OR target STREQUAL get-graphics-offsets)
       set(OBS_EXECUTABLE_DESTINATION "${OBS_DATA_DESTINATION}/obs-plugins/win-capture")
 
