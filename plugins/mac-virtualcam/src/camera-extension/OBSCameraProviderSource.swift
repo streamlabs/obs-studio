5be718a3ab95ef472252afc126132db90a14fa60
Richard <rosborne255@yahoo.com>
2025-08-28 16:18:07 -0500

    update to accept optional ARCH param + rename virtual camera (#668)
    * update to accept optional arch param
    
    * add -h,--help option incase its run manually
    
    * rename OBS Virtual Camera -> Streamlabs Virtual Camera

--- plugins/mac-virtualcam/src/camera-extension/OBSCameraProviderSource.swift
+++ plugins/mac-virtualcam/src/camera-extension/OBSCameraProviderSource.swift
@@ -19,7 +19,7 @@ class OBSCameraProviderSource: NSObject, CMIOExtensionProviderSource {
 
         provider = CMIOExtensionProvider(source: self, clientQueue: clientQueue)
         deviceSource = OBSCameraDeviceSource(
-            localizedName: "OBS Virtual Camera",
+            localizedName: "Streamlabs Virtual Camera",
             deviceUUID: deviceUUID,
             sourceUUID: sourceUUID,
             sinkUUID: sinkUUID)
