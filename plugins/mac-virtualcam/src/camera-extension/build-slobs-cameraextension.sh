62320c2d9fd3883552376ac066d368f1fb7acbee
Richard Osborne <rosborne255@yahoo.com>
2025-08-28 12:41:18 -0500

    add -h,--help option incase its run manually

--- plugins/mac-virtualcam/src/camera-extension/build-slobs-cameraextension.sh
+++ plugins/mac-virtualcam/src/camera-extension/build-slobs-cameraextension.sh
@@ -1,4 +1,24 @@
 # Use custom SLOBS CmakeLists.txt file which directly builds the SystemExtension (instead of the default CMakeLists.txt file)
+function display_usage {
+  echo "Usage: $(basename "$0") [OPTIONS]"
+  echo ""
+  echo "Description: This script builds the camera extension for slobs."
+  echo ""
+  echo "Options:"
+  echo "  -h, --help        Display this help message and exit."
+  echo "  arm64             set CMAKE_OSX_ARCHITECTURES to arm64"
+  echo "  x86_64            set CMAKE_OSX_ARCHITECTURES to x86_64"
+  echo ""
+  echo "Examples:"
+  echo "  $(basename "$0") arm64"
+  echo "  $(basename "$0") x86_64"
+  echo ""
+  exit 0
+}
+
+if [[ ( "$1" == "--help" ) || ( "$1" == "-h" ) ]]; then
+  display_usage
+fi
 
 cp -v ../../../../CMakePresets.json ./  # copy this file locally. We need to reuse the VIRTUALCAM_UUIDs
 
@@ -12,7 +32,7 @@ cmake_args=()
 
 if [ $# -ge 1 ]; then
   cmake_args+=(-DCMAKE_OSX_ARCHITECTURES="$1")
-done
+fi
 
 # Build the project using the custom SLOBS build.
 cmake --preset macos "${cmake_args[@]}"
