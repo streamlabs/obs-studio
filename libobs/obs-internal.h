e982ea756427ded9cef7c9bb64b4155ab1f0bcdd
Aleksandr Voitenko <avoitenko@logitech.com>
2026-03-20 16:21:43 +0000

    Updating audio output safely during reset and shutdown

--- libobs/obs-internal.h
+++ libobs/obs-internal.h
@@ -394,6 +394,11 @@ struct audio_monitor;
 
 struct obs_core_audio {
 	audio_t *audio;
+	// These 3 values are not present in the original OBS code. They serve as a main audio parameters cache, 
+	// allowing audio processing to finish the current iteration even if the global audio object has been destroyed.
+	uint32_t samples_per_sec;
+	enum speaker_layout speakers;
+	size_t channels;
 
 	DARRAY(struct obs_source *) render_order;
 	DARRAY(struct obs_source *) root_nodes;
