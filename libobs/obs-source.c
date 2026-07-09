42c82914525e65147c8ffa16e0f7a27a4c4717f6
Richard Osborne <rosborne255@yahoo.com>
2026-07-09 14:00:05 -0500

    call gs_texrender_reset() when rendering_mode changes

--- libobs/obs-source.c
+++ libobs/obs-source.c
@@ -4485,6 +4485,7 @@ bool obs_source_process_filter_begin_with_color_space(obs_source_t *filter, enum
 			gs_texrender_destroy(filter->filter_texrender);
 			filter->filter_texrender = NULL;
 			filter->filter_texrender_canvas = NULL;
+			filter->filter_texrender_mode = OBS_MAIN_VIDEO_RENDERING;
 		}
 	}
 
@@ -4496,9 +4497,12 @@ bool obs_source_process_filter_begin_with_color_space(obs_source_t *filter, enum
 		obs && obs->video_rendering_mix
 			? obs->video_rendering_mix->canvas_ovi
 			: NULL;
-	if (filter->filter_texrender_canvas != cur_canvas) {
+	enum obs_video_rendering_mode cur_mode = obs_get_video_rendering_mode();
+	if (filter->filter_texrender_canvas != cur_canvas ||
+	    filter->filter_texrender_mode != cur_mode) {
 		gs_texrender_reset(filter->filter_texrender);
 		filter->filter_texrender_canvas = cur_canvas;
+		filter->filter_texrender_mode = cur_mode;
 	}
 
 	if (gs_texrender_begin_with_color_space(filter->filter_texrender, cx, cy, space)) {
